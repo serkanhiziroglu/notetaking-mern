06f67661720379b7c4d7be0d95631b3b277050a0
shingggk <kiseshingo@cityuniversity.edu>
2023-06-04 18:06:15 -0700

    note body based on priority level

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -18,9 +18,8 @@ const Note = ({
   onDeleteNodeClicked,
   className,
 }: NoteProps) => {
-  console.log(note);
   const { title, text, createdAt, updatedAt, priority } = note;
-  // console.log(priority);
+
   let createdUpdatedText: string;
   if (updatedAt > createdAt) {
     createdUpdatedText = 'Updated: ' + formatDate(updatedAt);
@@ -28,9 +27,20 @@ const Note = ({
     createdUpdatedText = 'Created: ' + formatDate(createdAt);
   }
 
+  let priorityClass = '';
+  if (priority >= 3) {
+    priorityClass = styles.lowPriority;
+  }
+  if (priority >= 6 && priority >= 4) {
+    priorityClass = styles.mediumPriority;
+  }
+  if (priority >= 7) {
+    priorityClass = styles.highPriority;
+  }
+
   return (
     <Card
-      className={`${styles.noteCard} ${className}`}
+      className={`${styles.noteCard} ${priorityClass} ${className}`}
       onClick={() => onNoteClicked(note)}
     >
       <Card.Body className={styles.cardBody}>
@@ -54,75 +64,3 @@ const Note = ({
 };
 
 export default Note;
-
-// import { Note as NoteModel } from '../models/note';
-// import Card from 'react-bootstrap/Card';
-// import styles from '../styles/Note.module.css';
-// import { formatDate } from '../utils/formatDate';
-// import { MdDelete } from 'react-icons/md';
-// import styleUtils from '../styles/utils.module.css';
-
-// interface NoteProps {
-//   note: NoteModel;
-//   onNoteClicked: (note: NoteModel) => void;
-//   onDeleteNodeClicked: (node: NoteModel) => void;
-//   className?: string;
-// }
-
-// const Note = ({
-//   note,
-//   onNoteClicked,
-//   onDeleteNodeClicked,
-//   className,
-// }: NoteProps) => {
-//   const { title, text, createdAt, updatedAt, priority } = note;
-
-//   let createdUpdatedText: string;
-//   if (updatedAt > createdAt) {
-//     createdUpdatedText = 'Updated: ' + formatDate(updatedAt);
-//   } else {
-//     createdUpdatedText = 'Created: ' + formatDate(createdAt);
-//   }
-
-//   let priorityClass = '';
-
-//   switch (true) {
-//     case priority >= 1 && priority <= 3:
-//       priorityClass = styles.lowPriority;
-//       break;
-//     case priority >= 4 && priority <= 7:
-//       priorityClass = styles.mediumPriority;
-//       break;
-//     case priority >= 8 && priority <= 10:
-//       priorityClass = styles.highPriority;
-//       break;
-//     default:
-//       break;
-//   }
-
-//   return (
-//     <Card
-//       className={`${styles.noteCard} ${priorityClass} ${className}`}
-//       onClick={() => onNoteClicked(note)}
-//     >
-//       <Card.Body className={styles.cardBody}>
-//         <Card.Title className={styleUtils.flexCenter}>
-//           {title}
-//           <MdDelete
-//             className='text-muted ms-auto'
-//             onClick={(e) => {
-//               onDeleteNoteClicked(note);
-//               e.stopPropagation();
-//             }}
-//           />
-//         </Card.Title>
-//         <Card.Text className={styles.cardText}>{text}</Card.Text>
-//       </Card.Body>
-//       <Card.Footer className={styles.cardFooter}>
-//         {createdUpdatedText}
-//       </Card.Footer>
-//     </Card>
-//   );
-// };
-
-// export default Note;
