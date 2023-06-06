c4a6a71893a7495c4910d4ae17a9bd1947829a98
shingggk <kiseshingo@cityuniversity.edu>
2023-06-05 17:27:07 -0700

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,51 +1,65 @@
-import "dotenv/config";
-import express, { Request, Response, NextFunction } from "express";
-import notesRoutes from './routes/notes'
-import usersRoutes from './routes/users'
-import morgan from 'morgan'
-import createHttpError, { isHttpError } from "http-errors";
-import session from 'express-session'
-import env from "./util/validateEnv"
-import MongoStore from "connect-mongo";
-import { requiresAuth } from "./middleware/auth";
-
-
-
+import 'dotenv/config';
+import express, { Request, Response, NextFunction } from 'express';
+import notesRoutes from './routes/notes';
+import usersRoutes from './routes/users';
+import morgan from 'morgan';
+import createHttpError, { isHttpError } from 'http-errors';
+import session from 'express-session';
+import env from './util/validateEnv';
+import MongoStore from 'connect-mongo';
+import { requiresAuth } from './middleware/auth';
+import path from 'path';
 
 const app = express();
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(session({
+
+const _dirname = path.dirname('');
+const buildpath = path.join(_dirname, '../frontend/build');
+app.use(express.static(buildpath));
+
+app.get('/*', function (req, res) {
+  res.sendFile(
+    path.join(__dirname, '../frontend/build/index.html'),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(
+  session({
     secret: env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 60 * 60 * 1000,
+      maxAge: 60 * 60 * 1000,
     },
     rolling: true,
     store: MongoStore.create({
-        mongoUrl: env.MONGO_CONNECTION_STRING
+      mongoUrl: env.MONGO_CONNECTION_STRING,
     }),
-}));
-app.use('/api/notes', requiresAuth, notesRoutes)
-app.use('/api/users', usersRoutes)
-
+  })
+);
+app.use('/api/notes', requiresAuth, notesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-    next(createHttpError(404, "Endpoint not found!"))
-})
+  next(createHttpError(404, 'Endpoint not found!'));
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-    console.error(error)
-    let errorMessage = "An unknown error occured"
-    let statusCode = 500
-    if (isHttpError(error)) {
-        statusCode = error.status
-        errorMessage = error.message
-    }
-    res.status(statusCode).json({ error: errorMessage })
-})
-
+  console.error(error);
+  let errorMessage = 'An unknown error occured';
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
+});
 
 export default app;
